4303b2c5dba12456d5871ab7c78b40e2d627de63
PythEch <pythech.tr@gmail.com>
2014-01-12 13:02:11 +0200

    Netty Support
    https://github.com/Triforce1/Icew1nd/issues/2#issuecomment-32108884

--- __init__.py
+++ __init__.py
@@ -1,2 +1,9 @@
 import site, os, sys, inspect
 site.addsitedir(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
+
+
+if 'java' in sys.platform.lower():
+    wd = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'netty')
+    for jar in os.listdir(wd):
+        sys.path.append(os.path.join(wd, jar))
+    
