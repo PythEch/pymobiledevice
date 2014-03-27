0be4a17461f62481055303dc8dc9cce449a0dcee
PythEch <pythech.tr@gmail.com>
2014-03-27 20:58:00 +0200

    Move libraries

--- __init__.py
+++ __init__.py
@@ -1,16 +1,12 @@
 import os
 import sys
-import platform
 import site
 import inspect
 
 try:
     from java.lang import System
 except ImportError:
-    if hasattr(platform, 'python_implementation'): # Added in Python 2.6
-        raise RuntimeError("Unsupported implementation: " + platform.python_implementation())
-    else:
-        raise RuntimeError("Only Jython is supported")
+    raise RuntimeError("Only Jython is supported.")
 
 # Disable SSL/TLS socket randomization that iOS doesn't like
 System.setProperty('jsse.enableCBCProtection', 'false')
@@ -18,6 +14,10 @@ System.setProperty('jsse.enableCBCProtection', 'false')
 # Working directory, a missing variable in Python
 # Should work in all cases
 wd = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+lib_dir = os.path.join(wd, 'libs')
+
+# Makes importing 3rd party libraries easier
+site.addsitedir(os.path.join(lib_dir, 'python'))
 
 # Jython doesn't have an OS constant
 os_name = System.getProperty('os.name').encode('ascii', 'ignore').lower()
@@ -25,15 +25,12 @@ os_name = System.getProperty('os.name').encode('ascii', 'ignore').lower()
 if 'windows' in os_name:
     pass
 elif 'os x' in os_name or 'linux' in os_name:
-    System.setProperty('org.newsclub.net.unix.library.path', os.path.join(wd, 'native'))
+    System.setProperty('org.newsclub.net.unix.library.path', os.path.join(lib_dir, 'native'))
 else:
     raise RuntimeError("Unsupported OS: " + os_name)
 
-# Makes importing 3rd party libraries easier
-site.addsitedir(wd)
-
-if len([s for s in sys.path if 'pymobiledevice\\jar\\' in s]) == 0:
-    jar_dir = os.path.join(wd, 'jar')
+jar_dir = os.path.join(lib_dir, 'java')
+if len([s for s in sys.path if jar_dir in s]) == 0:
     for jar in os.listdir(jar_dir):
         sys.path.append(os.path.join(jar_dir, jar))
 #else:
